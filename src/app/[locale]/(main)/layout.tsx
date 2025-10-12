a39c392ce09fdd90b6f49f5b632c261db09ca3f7
Amr Salah <amrro.salah@gmail.com>
2025-10-12 17:43:32 +0200

    try check4

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -1,11 +1,10 @@
 import Footer from '@/components/layout/footer/Footer';
 import Navbar from '@/components/layout/navbar/Navbar';
 
-export default function MainLayout({ children }: { children: React.ReactNode }) {
+export default function MainLayout({ children }) {
   return (
     <div className='flex min-h-screen flex-col'>
       <Navbar />
-
       <main className='flex-grow'>{children}</main>
       <Footer />
     </div>
