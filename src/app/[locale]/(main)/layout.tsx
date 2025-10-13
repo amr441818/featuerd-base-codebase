0cd6565c5c9c114aa4ccfd0ae1750ace1703285c
Amr Salah <amrro.salah@gmail.com>
2025-10-13 11:22:08 +0200

    fic

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -1,12 +1,13 @@
 import Footer from '@/components/layout/footer/Footer';
 import Navbar from '@/components/layout/navbar/Navbar';
+import { ReactNode } from 'react';
 
-export default function MainLayout({ children }) {
+export default function MainLayout({ children }:{children:ReactNode}) {
   return (
     <div className='flex min-h-screen flex-col'>
       <Navbar />
       <main className='flex-grow'>{children}</main>
-      <Footer />
+           <Footer />
     </div>
   );
 }
